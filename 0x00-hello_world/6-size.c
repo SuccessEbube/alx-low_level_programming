631249dced19059d3039fe629b7d50c3b573f672
SuccessEbube <successebubechukwu123@gmail.com>
2023-03-20 22:57:00 +0000

    size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  main - main function
+ *
+ *  Return: o on success
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)", sizeof(char))\n;
+	printf("Size of  int: %lu byte(s)", sizeof(int))\n;
+	printf("Size of a long int: %lu byte(s)", sizeof(long int))\n;
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int))\n;
+	printf("Size of a float: %lu byte(s)", sizeof(float))\n;
+	return (0):
+}
+
