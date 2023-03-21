63d661540d494e3eefa3f4ca86b78fe3a3770c14
SuccessEbube <successebubechukwu123@gmail.com>
2023-03-21 00:04:15 +0000

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * return: 1.
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19".
+ * Return: Always 1.
  */
 
 int main(void)
