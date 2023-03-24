c04b03ba9777b27bc23ba160a05c3533c5bcfad3
SuccessEbube <successebubechukwu123@gmail.com>
2023-03-24 01:12:53 +0000

    last part

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include "main.h"
 
+/**
+ * main - takes a date accounting a leap year.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int month;
+	int day;
+	int year;
+
+	month = 2;
+	day = 29;
+	year = 2000;
+
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+
+	return (0);
+}
