ec4731563d8e8a1d13cbddd5637e33a9099d04c2
SuccessEbube <successebubechukwu123@gmail.com>
2023-03-29 01:02:39 +0000

    updated

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  puts_half - a function that prints half a string 
+ *  puts_half - a function that prints half a string
  *  if odd len, n = (length_of_the_string - 1) / 2
  *  @str: input
  *  Return: half of input
