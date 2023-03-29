8c008850c01039798b06674942752ab39b6b4b1f
SuccessEbube <successebubechukwu123@gmail.com>
2023-03-29 00:47:28 +0000

    update

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string, followed by a new line, to stdout
- * @str: string to print
- * Return: void
+ * _puts - Prints a string, followed by a new line, to stdout.
+ * @str: string to print.
+ * Return: void.
  */
 
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
-	{
- 	putchar(*str);
-	}
-	putchar('\n');
+for ( ; *str != '\0'; str++)
+{
+putchar(*str);
+}
+putchar('\n');
 }
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * char *_strcpy - a function that copies the string pointed to by src. 
+ * char *_strcpy - a function that copies the string pointed to by src.
  * @dest: Destination value.
  * @src: source value.
  * Return: string.
  */
 
-char *_stropy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 int i = 0;
 int x = 0;
